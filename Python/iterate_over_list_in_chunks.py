db05a8472d55ddb7ad46bd5da58b663e2bab781c
Rakesh Sukla <rsukla@handy.com>
2016-04-01 17:54:03 -0400

    another clever way to use grouper

--- Python/iterate_over_list_in_chunks.py
+++ Python/iterate_over_list_in_chunks.py
@@ -4,6 +4,21 @@ from string import ascii_letters
 def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return izip_longest(*args, fillvalue=fillvalue)
-
 print list(grouper(ascii_letters, 3))
+
+'''
+>>> l = range(1,11)
+>>> i = iter(l)
+>>> zip(i, i)
+[(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)]
+>>> zip(i, i, i)
+[]
+>>> zip(i, i)
+[]
+>>> l = range(1,11)
+>>> i = iter(l)
+>>> zip(i, i, i)
+[(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+>>>
+'''
 
